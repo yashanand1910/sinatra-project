4f0ceccecff887eca626acf03e897e14f7995490
Yash Anand <yashanand1910@gmail.com>
2017-01-25 19:28:39 +0530

    Some syntax fixes

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -6,7 +6,7 @@ require 'sinatra/reloader' if development?
 get '/' do
 	erb :index
 end
-post '/result' do
+post '/' do
 	string = params['string']
 	crypt = encrypt(string, 4)
 	erb :result, :local => { :string => string, :crypt => crypt }
